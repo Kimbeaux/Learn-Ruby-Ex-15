e72e0b8d5ebe8fbcf42d37a55311218e8d40ce73
Kimbeaux <KimbeauxE@verizon.net>
2012-03-14 11:51:56 -0400

    Added ECs, except 6.

--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,27 @@
+# unpacks the name of the text file from the command line (after the program name) into variable filename
 filename = ARGV.first
 
+# loads variable prompt with "> "
 prompt = "> "
+# opens filename and puts contents in variable txt
 txt = File.open(filename)
 
+# print text w/ filename
 puts "Here's your file: #{filename}"
+# print content of txt
 puts txt.read()
 
-puts "I'll also ask you to type it again:"
-print prompt
-file_again = STDIN.gets.chomp()
+# print text
+# puts "I'll also ask you to type it again:"
+# print "> "
+# print prompt
+# load variable file_again w/ arguement from keyboard, truncating carriage return
+# file_again = STDIN.gets.chomp()
 
-txt_again = File.open(file_again)
+# opens file_again and puts content in txt_again
+# txt_again = File.open(file_again)
 
-puts txt_again.read()
+# print content of txt_again
+# puts txt_again.read()
+
+txt.close
